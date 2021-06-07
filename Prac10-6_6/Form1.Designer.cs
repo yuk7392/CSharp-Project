7ab57967d8adaa86c0c57c45064631adf379092e
yuk7392 <2sh0430@naver.com>
2021-06-07 11:13:47 +0900

    Prac 10-6_6

---
+++ Prac10-6_6/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Prac10_6_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 331);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Prac10-6_6/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prac10_6_6
+{
+    public partial class Form1 : Form
+    {
+        bool shift = false, alt = false, ctrl = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.F1 && (e.Shift)) MessageBox.Show(e.KeyCode.ToString()+"+Shift");
+            if (e.KeyCode == Keys.F1 && (e.Alt)) MessageBox.Show(e.KeyCode.ToString() + "+Alt");
+            if (e.KeyCode == Keys.F1 && (e.Control)) MessageBox.Show(e.KeyCode.ToString() + "+Control"); 
+
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.Shift) shift = true;
+            if (e.Control) ctrl = true;
+            if (e.Alt) alt = true;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if (shift)
+                MessageBox.Show(e.KeyCode.ToString() + "+Shift");
+            if (ctrl)
+                MessageBox.Show(e.KeyCode.ToString() + "+Control");
+            if (alt)
+                MessageBox.Show(e.KeyCode.ToString() + "+Alt");
+
+            if (!shift && !ctrl && !alt)
+                MessageBox.Show(e.KeyCode.ToString());
+
+            shift = false;
+            ctrl = false;
+            alt = false;
+
+        }
+    }
+}
