a7dd09f28449fdafad3ff53de6c5ff71c703baa8
yuk7392 <2sh0430@naver.com>
2021-06-07 11:14:51 +0900

    Prac 10-6_3

---
+++ Prac10-6_3/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Prac10_6_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "마우스로 사각형을 그리시오";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 300);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "사각형 그리기";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Prac10-6_3/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prac10_6_3
+{
+    public partial class Form1 : Form
+    {
+
+        int firstX = 0;
+        int firstY = 0;
+        int lastX = 0;
+        int lastY = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(ForeColor);
+
+            g.DrawRectangle(pen, firstX, firstY, firstX > lastX ? firstX-lastX : lastX-firstX, firstY > lastY ? firstY-lastY : lastY - firstY);
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            firstX = e.X;
+            firstY = e.Y;
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            lastX = e.X;
+            lastY = e.Y;
+
+            Invalidate();
+
+        }
+    }
+}
